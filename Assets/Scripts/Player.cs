2b2729129d23b016465b0568dc176b2fe70823a6
Nivaud Gabriel <g.nivaud@student.isartdigital.com>
2020-01-31 22:13:59 +0100

    Player controle start

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private PlayerController controller;
+    [SerializeField] private PlayerSettings settings;
+
+    private Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity = new Vector3(controller.HorizontalAxis * settings.Speed, 0, controller.VerticalAxis * settings.Speed);
+
+        transform.position += velocity;
+    }
+}

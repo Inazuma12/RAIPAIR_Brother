4d5a6532265947119e8d39a823687e83f07a248b
Nivaud Gabriel <g.nivaud@student.isartdigital.com>
2020-01-31 23:39:35 +0100

    2 joueurs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,7 @@ public class Player : MonoBehaviour
     [SerializeField] private PlayerSettings settings;
 
     private Vector3 velocity;
+    private bool isMobile = true;
 
     // Start is called before the first frame update
     void Start()
@@ -18,8 +19,20 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        velocity = new Vector3(controller.HorizontalAxis * settings.Speed, 0, controller.VerticalAxis * settings.Speed);
+        velocity = new Vector3(controller.HorizontalAxis * settings.Speed, 0, controller.VerticalAxis * settings.Speed );
 
-        transform.position += velocity;
+        if (isMobile) transform.position += velocity;
+        else if (!isMobile && controller.VerticalAxis < 0) transform.position += new Vector3(0, 0, controller.VerticalAxis * settings.Speed);
+        else if (!isMobile) transform.position += new Vector3(controller.HorizontalAxis * settings.Speed, 0, 0);
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    { 
+        isMobile = false;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isMobile = true;
     }
 }

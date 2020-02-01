bce32e9090fd86326cc322f6f9a0c91988e2dac9
Nivaud Gabriel <g.nivaud@student.isartdigital.com>
2020-02-01 17:40:20 +0100

    Start Stockage Block

--- Assets/Scripts/Block/StorageBlock.cs
+++ Assets/Scripts/Block/StorageBlock.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class StorageBlock : PickUpDipositeBlock
 {
-    
+    public override PickableObject PickUp()
+    {
+        if (!ownPickableObject) return null;
+
+        PickableObject pickableObject = ownPickableObject;
+        ownPickableObject = null;
+        return pickableObject;
+    }
+
+    public override void Diposide(PickableObject pickableObject)
+    {
+        if(ownPickableObject) ownPickableObject = pickableObject;
+    }
 
 }

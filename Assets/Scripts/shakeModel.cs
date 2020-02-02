6f908300629b5167e269265eaa3ca7fceb0f681c
EDJAntimatter <quentin.croizit@gmail.com>
2020-02-02 12:32:13 +0100

    real replace on object fix

--- Assets/Scripts/shakeModel.cs
+++ Assets/Scripts/shakeModel.cs
@@ -34,7 +34,7 @@ public class shakeModel : MonoBehaviour
             modelTransformBefore = modelToShake.transform.position;
             StartCoroutine(_modelShake(0.1f));
             wasShake = true;
-            pickableObject.transform.position = new Vector3(transform.position.x, transform.position.y, pickableObject.transform.position.z);
+            pickableObject.transform.position = new Vector3(transform.position.x, pickableObject.transform.position.y, transform.position.z);
         }
         if (pickableObject == null)
         {
